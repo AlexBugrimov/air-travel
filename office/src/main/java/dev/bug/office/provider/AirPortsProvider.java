d0e6dbbe3c8c22c567a46b175e0e1b2eff9e19f2
Alex <bugrimov_a@bk.ru>
2021-12-28 15:13:30 +0300

    👷 Added AirPortsProvider

---
+++ office/src/main/java/dev/bug/office/provider/AirPortsProvider.java
@@ -0,0 +1,47 @@
+package dev.bug.office.provider;
+
+import dev.bug.common.AirPort;
+import dev.bug.common.RoutePoint;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Slf4j
+@Getter
+@Component
+@ConfigurationProperties("application")
+public class AirPortsProvider {
+
+    private final List<AirPort> ports = new ArrayList<>();
+
+    public AirPort findAirPortAndRemoveBoard(String boardName) {
+        AtomicReference<AirPort> res = new AtomicReference<>();
+        ports.stream().filter(airPort -> airPort.getBoards().contains(boardName))
+                .findFirst()
+                .ifPresent(airPort -> {
+                    airPort.removeBoard(boardName);
+                    res.set(airPort);
+                });
+        return res.get();
+    }
+
+    public RoutePoint getRoutePoint(String airPortName) {
+        AirPort airPort = getAirPort(airPortName);
+        return RoutePoint.builder()
+                .name(airPort.getName())
+                .x(airPort.getX())
+                .y(airPort.getY())
+                .build();
+    }
+
+    private AirPort getAirPort(String airPortName) {
+        return ports.stream().filter(airPort -> airPort.getName().equals(airPortName))
+                .findFirst()
+                .orElse(AirPort.builder().build());
+    }
+}
